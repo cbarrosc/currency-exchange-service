c6c06bb8055b1cb990646dc02d2cde22e2b0fe96
cbarros <camilo.barros@globant.com>
2022-01-31 16:23:31 -0300

    added resilience4j for circuit breaker pattern

---
+++ src/main/java/com/globant/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package com.globant.microservice.currencyexchangeservice;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class CircuitBreakerController {
+
+
+    @GetMapping("/sample-api")
+    @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi(){
+        log.info("Sample Api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080-some-dummy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception ex){
+        return "fallback-response";
+    }
+
+}

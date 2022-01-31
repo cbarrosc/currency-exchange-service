96535ac8ccb84996bb02cac0c0edc97ffff8eaf5
cbarros <camilo.barros@globant.com>
2022-01-31 16:47:27 -0300

    configured rate limiter, bulk head and circuit breaker properties

--- src/main/java/com/globant/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/globant/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.globant.microservice.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +17,14 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name="sample-api", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+//    @RateLimiter(name="default")
+    @Bulkhead(name="sample-api")
     public String sampleApi(){
         log.info("Sample Api call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080-some-dummy-url", String.class);
-        return forEntity.getBody();
+       /* ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080-some-dummy-url", String.class);
+        return forEntity.getBody();*/
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex){

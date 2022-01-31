8be34eccd53032f350c643c3f83b2bc2d3080902
cbarros <camilo.barros@globant.com>
2022-01-31 11:42:03 -0300

    initialized h2 database and jpa repository

--- src/main/java/com/globant/microservice/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/globant/microservice/currencyexchangeservice/CurrencyExchange.java
@@ -1,16 +1,23 @@
 package com.globant.microservice.currencyexchangeservice;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private Long id;
+    @Column(name="currency_from")
     private String from;
+    @Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
